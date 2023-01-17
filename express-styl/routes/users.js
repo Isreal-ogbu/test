01ba6d3d0407f675b492f5e840b28b41de8ae74b
Ogbu Isreal <ogbuisreal22@gmail.com>
2023-01-17 13:22:33 +0100

    Refactored code base

--- express-styl/routes/users.js
+++ express-styl/routes/users.js
@@ -9,42 +9,14 @@ const knex = require('knex')(knexConfig.development)
 
 router.route('/')
 
-.all((req, res, next) => {
-  next()
-})
-.get((req, res) => {
-  return readUsers.users.getUsers()
-})
-
-.post((req, res) => {
-  return readUsers.users.postUsers()
-})
+.all((req, res, next) => {next()})
+.get(readUsers.getUsers)
+.post(readUsers.postUsers)
 
 router.route('/:id')
 
-.all((req,res, next) => {
-  next()
-})
+.all((req,res, next) => {next()})
+.get(readUsers.readUserById)
+.put(readUsers.changeUserDetails)
 
-.get((req,res) => {
-  knex('dusers')
-  .select ({
-    id: "id",
-    name: "name"
-  })
-  .where({id : req.params.id})
-  .then((user)=> {
-    return res.json(user)
-  })
-})
-.put((req, res)=> {
-  knex('dusers')
-  .update({
-    name : req.body.name
-  })
-  .where({id : req.params.id})
-  .then(()=> {
-    res.send("changed")
-  })
-})
 module.exports = router;
--- express-styl/serices/users.js
+++ express-styl/serices/users.js
@@ -3,49 +3,42 @@ let knex = require('knex')(config.development)
 
 
 module.exports = users = {
-    getUsers : () => {
-        knex('dusers')
-    .select({
-      id: 'id',
-      name: 'name',
-      email: "email"
-    })
-    .then((user) => {
-      return res.json(user);
-    })
-    .catch((err) => {
-      console.error(err);
-      return res.json({success: false, message: 'An error occurred, please try again later.'});
-    })
-    },
+    getUsers : async (req, res) => {
+       await knex('dusers')
+        .select({id: 'id',name: 'name',email: "email"})
+        .then((user) => {return res.json(user);})
+        .catch((err) => {console.error(err); return res.json({success: false, message: 'An error occurred, please try again later.'});})
+        },
 
-    postUsers: () => {
+    postUsers: async (req, res) => {
         const name = req.body.name ? req.body.name : '';
-                email = req.body.email ? req.body.email : '';
-                password = req.body.password ? req.body.password : '';
+        const email = req.body.email ? req.body.email : '';
+        const password = req.body.password ? req.body.password : '';
+        if (!name) {return res.status(400).json({success: false, message: 'Name is required'});}
+        await knex('dusers')
+            .insert({name, email, password})
+            .then((id) => {
+            knex('dusers')
+                .select({ id: 'id', name: 'name'})
+                .where({id: id[0]})
+                .then((user1) => { return res.status(200).json(user1[0]);})})
+                .catch((err) => { console.error(err); return res.status(400).json({success: false, message: 'An error occurred, please try again later.'});});
+        },
 
+    readUserById: async (req, res) => {
+        await knex('dusers')
+        .select({name: "name", email: "email"})
+        .where({id : req.params.id})
+        .then((result) => { return res.json(result)})
+        .catch((err) => { return res.status(400).send('Invalid')})
+        },
 
-if (!name) {
-    return res.status(400).json({success: false, message: 'Name is required'});
-}
+    changeUserDetails: async (req, res) => {
+        await knex('dusers')
+        .update({name: req.body.name})
+        .where({id: req.params.id})
+        .then((result)=> { return res.json(result)})
+        .catch((err) => { return res.status(400).send("No change occured")})
+        }
 
-knex('dusers')
-    .insert({name, email, password})
-    .then((id) => {
-    //get user by id
-    knex('dusers')
-        .select({
-        id: 'id',
-        name: 'name'
-    })
-      .where({id: id[0]})
-      .then((user1) => {
-      return res.status(200).json(user1[0]);
-    })
-})
-    .catch((err) => {
-    console.error(err);
-    return res.status(400).json({success: false, message: 'An error occurred, please try again later.'});
-});
-}
 };
